cd65d207128af4af15880ab8b4df050852db38f3
torres.nguyen <torres.nguyen@starixvn.com>
2021-06-03 23:16:56 +0700

    complete login

--- helpers/responses.helper.js
+++ helpers/responses.helper.js
@@ -1,9 +1,5 @@
 const {
     REGISTER_ALREADY_USERNAME,
-    LOGIN_FAIL_PASSWORD,
-    LOGIN_FAIL_USERNAME,
-    REGISTER_ALREADY_EMAIL,
-    LOGIN_FAIL_ROLE,
 } = require("../constants/error-code.constant");
 
 const UsersResponses = {
@@ -27,50 +23,7 @@ const UsersResponses = {
             statusCode: 200,
             payload: {
                 msg: "Email is already exist",
-                error: REGISTER_ALREADY_EMAIL,
-            },
-        };
-    },
-    loginSuccess(token) {
-        return {
-            statusCode: 200,
-            payload: {
-                accessToken: token,
-            },
-        };
-    },
-    loginNotExistUsername() {
-        return {
-            statusCode: 200,
-            payload: {
-                msg: "Username is not exist",
-                errorCode: LOGIN_FAIL_USERNAME,
-            },
-        };
-    },
-    loginNotCorrectRole() {
-        return {
-            statusCode: 200,
-            payload: {
-                msg: "Permission is not valid",
-                errorCode: LOGIN_FAIL_ROLE,
-            },
-        };
-    },
-    loginNotCorrectPassword() {
-        return {
-            statusCode: 200,
-            payload: {
-                msg: "Password was wrong",
-                errorCode: LOGIN_FAIL_PASSWORD,
-            },
-        };
-    },
-    getAllSuccess(users) {
-        return {
-            statusCode: 200,
-            payload: {
-                users,
+                error: REGISTER_ALREADY_USERNAME,
             },
         };
     },
