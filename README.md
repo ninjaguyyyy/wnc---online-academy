93a9b62ab2f3bfb3b498175076780fc1832d9e2d
LÊ ĐOÀN CÔNG ẢNH <conganhhcmus@gmail.com>
2021-08-15 18:43:58 +0700

    [add] - [add ci-cd]

--- README.md
+++ README.md
@@ -1,7 +1,11 @@
 # wnc-online-academy
+
 ## Host
+
 https://wnc-online-academy-21.herokuapp.com/
+
 ## Chức năng làm đc
+
 - Đăng ký
   - Password được mã hóa
   - Có xác nhận OTP bằng email và có expired time
--- helpers/hashing.helper.js
+++ helpers/hashing.helper.js
@@ -1,14 +1,14 @@
-const bcrypt = require("bcryptjs");
+const bcrypt = require('bcryptjs');
 
 const hashingManager = {};
 
 const generateHashPassword = (passwordString) => {
-    const salt = 10;
-    return bcrypt.hashSync(passwordString, salt);
+  const salt = 10;
+  return bcrypt.hashSync(passwordString, salt);
 };
 
 const checkValidPassword = (rawPassword, hashPassword) => {
-    return bcrypt.compareSync(rawPassword, hashPassword);
+  return bcrypt.compareSync(rawPassword, hashPassword);
 };
 
 hashingManager.generateHashPassword = generateHashPassword;
